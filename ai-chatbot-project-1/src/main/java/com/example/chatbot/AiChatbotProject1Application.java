327a85ce8f65c985a66fcb680b0828ce6c2244ec
mahendra <mahendrasing0115@gmail.com>
2026-01-16 01:37:23 +0530

    first commit

---
+++ ai-chatbot-project-1/src/main/java/com/example/chatbot/AiChatbotProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatbotProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatbotProject1Application.class, args);
+	}
+
+}
---
+++ ai-chatbot-project-1/src/main/java/com/example/chatbot/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.example.chatbot.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.chatbot.service.ChatService;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+	private ChatService chatService;
+	
+	public ChatController (ChatService chatService) {
+		this.chatService = chatService;
+	}
+	
+	
+	@PostMapping
+	public String chat(@RequestBody String message) {
+		return chatService.ask(message);
+	}
+	
+	
+	
+	
+	
+}
