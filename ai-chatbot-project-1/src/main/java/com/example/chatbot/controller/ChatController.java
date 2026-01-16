f879631a629f914f307096ef0695b835ce8bc852
mahendra <mahendrasing0115@gmail.com>
2026-01-16 16:12:10 +0530

    Added Server-Sent Streaming response using Flux

--- ai-chatbot-project-1/src/main/java/com/example/chatbot/controller/ChatController.java
+++ ai-chatbot-project-1/src/main/java/com/example/chatbot/controller/ChatController.java
@@ -1,11 +1,17 @@
 package com.example.chatbot.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.example.chatbot.service.ChatService;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/chat")
@@ -24,6 +30,18 @@ public class ChatController {
 	}
 	
 	
+//	@PostMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//	public Flux<String> chatStream(@RequestBody String message) {
+//	    return chatService.askStreaming(message);
+//	}
+	
+	@GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> chatStream(@RequestParam String message) {
+	    return chatService.askStreaming(message);
+	}
+	
+	
+
 	
 	
 	
--- ai-chatbot-project-1/src/main/java/com/example/chatbot/service/ChatService.java
+++ ai-chatbot-project-1/src/main/java/com/example/chatbot/service/ChatService.java
@@ -1,23 +1,56 @@
 package com.example.chatbot.service;
 
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
 
 
 @Service
 public class ChatService {
-    private OllamaChatModel chatModel;
+	
+	private OllamaChatModel chatModel;
+	
+	
+	public ChatService(OllamaChatModel chatModel) {
+		 this.chatModel = chatModel;
+		 
+	}
+	
+	public String ask(String message) {
+		
+		return chatModel.call(message);
+	}
     
-    public ChatService(OllamaChatModel chatModel) {
-    	this.chatModel = chatModel;
+	
+	
+//  Streming Response  token by token 
+
+	public Flux<String> askStreaming(String message) {
+
+        Prompt prompt = new Prompt(message); // Pre-defined Spring AI class
+
+        return chatModel.stream(prompt)      // Flux<ChatResponse>
+                .map(chatResponse ->
+                        chatResponse.getResult()
+                                .getOutput()  
+                                .getText()
+
+                );
     }
     
-    public String ask(String message) {
-    	
-    	return chatModel.call(message);
-    }
-    
-    
-   
-    
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
